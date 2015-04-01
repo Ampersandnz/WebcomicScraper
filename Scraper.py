42da2e15a80bf3740508bdf19cd42f4e88a2c648
Michael Lo <nz.ampersand@gmail.com>
2015-04-02 09:55:51 +1300

    Finished argument parsing code and moved it into Scraper.py. Added ability to specify webcomic name, number of chapters, number of pages per chapter, and image url.

--- Scraper.py
+++ Scraper.py
@@ -3,26 +3,37 @@ __author__ = 'Michael'
 
 import urllib.request
 import os
+import argparse
+import re
 from pathlib import Path
 from multiprocessing.pool import Pool
 
-# TODO: Allow users to specify webcomic name (folder it'll be saved in).
-webcomic_name = "Unsounded"
-
-# TODO: Allow users to specify whether subsets of the comic are called volumes or chapters.
-subset_type = "Chapter"
-
-# TODO: Allow users to specify how many subsets there are.
-num_subsets = 11
-
-# TODO: Allow users to specify how many pages the largest subset has.
-pages_per_subset = 150
-
-# TODO: Allow users to specify format of urls (does not allow comic title, only subset and page number.
+webcomic_name = None
+subset_type = None
+num_subsets = None
+pages_per_subset = None
 
 
 def main():
-    home = os.getcwd()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', metavar='U', help='Comic page URL, with page and chapter/volume numbers substituted.')
+    parser.add_argument('name', metavar='N', help='Webcomic name.')
+    parser.add_argument('subsets', metavar='C', help='Number of chapters/volumes.')
+    parser.add_argument('pages', metavar='P', help='Maximum pages in a chapter.')
+
+    args = parser.parse_args()
+
+    global webcomic_name
+    webcomic_name = args.name
+
+    global num_subsets
+    num_subsets = int(args.subsets)
+
+    global pages_per_subset
+    pages_per_subset = int(args.pages)
+
+    url_as_list = split_url(args.url)
+
     for i in range(1, num_subsets + 1):
         download_dir = Path(os.getcwd() + "/" + webcomic_name)
 
@@ -30,16 +41,35 @@ def main():
             download_dir.mkdir()
         os.chdir(download_dir.name)
 
+        global subset_type
+
         urls = []
         for j in range(1, pages_per_subset + 1):
-            url = str("http://www.casualvillain.com/Unsounded/comic/ch" + str(i).zfill(2) + "/pageart/ch" + str(i).zfill(2) + "_" + str(j).zfill(2) + ".jpg")
-            urls.append(url)
+
+            numbered_url = ''
+
+            for sub_url in url_as_list:
+                if re.match('^p+$', sub_url):
+                    numbered_url += (str(j).zfill(len(sub_url)))
+                elif re.match('^c+$', sub_url):
+                    subset_type = "Chapter"
+                    numbered_url += (str(i).zfill(len(sub_url)))
+                elif re.match('^v+$', sub_url):
+                    subset_type = "Volume"
+                    numbered_url += (str(i).zfill(len(sub_url)))
+                else:
+                    numbered_url += (sub_url)
+
+            print("URL " + numbered_url + " added to queue.")
+
+            urls.append(numbered_url)
 
         with Pool(12) as p:
             p.map(download_file, urls)
 
         # Move downloaded files into a .cbr archive.
-        rar_name = ("\"Chapter " + str(i) + ".cbr\"")
+        # Will only work on Linux currently - too lazy for get it working on Windows.
+        rar_name = ("\"" + subset_type + " " + str(i) + ".cbr\"")
         os.system('rar m -m0 ' + rar_name + " *.jpg")
 
 
@@ -55,6 +85,24 @@ def download_file(url):
         if os.path.getsize(filename) < 25 * 1024:
             os.remove(filename)
 
+# Input url of a similar form to 'http://www.website/{vvv}/{p}.jpg' where v and p are placeholders indicating the number of digits in the volume/chapter and page numbers.
+# Example: http://www.casualvillain.com/Unsounded/comic/ch{cc}/pageart/ch{cc}_{pp}.jpg
+def split_url(url):
+
+    if not re.match('http(s)?://', url):
+        print("Url must begin with http:// or https://")
+        exit()
+
+#TODO: Check that the url is a direct link to an image file (too many formats, sort it out later)
+#if not re.match('\.jpg$', url):
+#    print("Url must end with .jpg (must be a direct link to the image file).")
+#    exit()
+
+    if not re.findall('\{p+\}', url):
+        print("Url must contain a page number placeholder!")
+        exit()
+
+    return re.split("[\{\}]", url)
 
 if __name__ == "__main__":
     main()
